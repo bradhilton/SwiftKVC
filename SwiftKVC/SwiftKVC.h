72b1e30d30ea3568f7ab1f9875758c1f124aa642
bradhilton <brad.hilton.nw@gmail.com>
2015-08-31 12:42:29 -0600

    Initial Commit

---
+++ SwiftKVC/SwiftKVC.h
@@ -0,0 +1,19 @@
+//
+//  SwiftKVC.h
+//  SwiftKVC
+//
+//  Created by Bradley Hilton on 8/31/15.
+//  Copyright © 2015 Skyvive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftKVC.
+FOUNDATION_EXPORT double SwiftKVCVersionNumber;
+
+//! Project version string for SwiftKVC.
+FOUNDATION_EXPORT const unsigned char SwiftKVCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftKVC/PublicHeader.h>
+
+
