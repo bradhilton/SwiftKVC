b4467a63f92890b9475cdde31880794cb5d082bc
Brad Hilton <brad.hilton.nw@gmail.com>
2017-09-26 09:00:56 -0600

    Share scheme, update playgrounds example

--- SwiftKVC.playground/Contents.swift
+++ SwiftKVC.playground/Contents.swift
@@ -17,7 +17,25 @@ class User : Object {
     
 }
 
+class SubUser : User {
+    
+    var email: String
+    
+    init(name: String, age: Int, email: String) {
+        self.email = email
+        super.init(name: name, age: age)
+    }
+    
+}
+
 let user = User(name: "Brad", age: 25)
 user["name"] = "Larry"
 assert(user["age"] as? Int == 25)
 assert(user.name == "Larry")
+
+let subUser = SubUser(name: "Jerry", age: 59, email: "jerry@face.com")
+subUser["email"] = "jerry@live.com"
+assert(subUser["email"] as? String == "jerry@live.com")
+// Doesn't work:
+// subUser["age"] = 60
+// assert(subUser["age"] as? Int == 60)
